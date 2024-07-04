e92dd376880825ca2e1cf7759d2335f06e624353
Jhonatan Rivera <jhontanjuarez723@gmail.com>
2024-07-04 13:50:25 -0500

    Actividad: Seleccionando el Tipo de Dato Adecuado en Java. Finalizada

---
+++ Clase 1 - Introducción a Java/TipoDeDatoAdecuadoEnJava.java
@@ -0,0 +1,11 @@
+// Actividad: Seleccionando el Tipo de Dato Adecuado en Java ---------------------------------------------------
+// Temperatura media de una ciudad durante el verano (en grados Celsius) `double` Permite decimales, necesario para temperaturas precisas.
+// Precio de un producto en una tienda (en dólares y centavos) `double` Maneja decimales, ideal para precios con centavos.
+// Nombre completo de una persona `String` Almacena texto y caracteres especiales.
+// Fecha de nacimiento de una persona `String` Permite almacenar fechas en formato de texto (e.g., "yyyy-MM-dd").
+// Número de habitantes de una ciudad `int` Adecuado para enteros, suficiente para la mayoría de ciudades.
+// Duración de una película (en minutos) `int` Suficiente para tiempos en minutos.
+// Si un artículo está disponible o no en una tienda (true/false) `boolean` Representa valores binarios, como disponible/no disponible.
+// Distancia entre dos ciudades (en kilómetros) `double` Permite decimales, necesario para distancias precisas.
+// Número de teléfono de una persona `String` Almacena números con guiones y paréntesis.
+// Cantidad de existencias de un producto en una tienda `int` Adecuado para cantidades enteras.

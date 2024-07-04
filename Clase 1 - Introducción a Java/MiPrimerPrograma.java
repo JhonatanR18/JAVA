a34f97b2b43bda9cacbd75ea9d45e82bfeaae106
Jhonatan Rivera <jhontanjuarez723@gmail.com>
2024-07-04 12:15:59 -0500

    Actividad: Imprimiendo un mensaje por consola. Finalizada

---
+++ Clase 1 - Introducción a Java/MiPrimerPrograma.java
@@ -0,0 +1,6 @@
+// Declaración de la clase MiPrimerPrograma
+public class MiPrimerPrograma {
+    public static void main(String[] args) throws Exception { // método principal main
+        System.out.println("Hola Mundo");  // Imprime un mensaje
+    }
+}

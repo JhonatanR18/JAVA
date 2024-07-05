573033c7db7411372bed1df781d124506dcf1a98
Jhonatan Rivera <jhontanjuarez723@gmail.com>
2024-07-05 13:51:55 -0500

    Actividad: Usando los operadores lógicos. Finalizada

--- Clase 1 - Introducción a Java/Operadores02.java
+++ Clase 1 - Introducción a Java/Operadores02.java
@@ -19,3 +19,27 @@ public class Operadores02 {
         números.close();
     }
 }
+// usando los operadores lógicos 02 AND (&&), OR (||) y NOT (!)
+class OperadoresLógicos {
+    public static void main(String[] args) {
+        // creamos una instancia scanner
+        Scanner números = new Scanner(System.in);
+        System.out.print("Ingrese un número: ");
+        int num1 = números.nextInt(); // pedimos el primer número
+        System.out.print("Ingrese otro número: ");
+        int num2 = números.nextInt(); // pedimos el segundo número
+        System.out.print("Ingrese el tercer número: ");
+        int num3 = números.nextInt(); // pedimos el tercer número
+        System.out.print("Ingrese el cuarto número: ");
+        int num4 = números.nextInt(); // pedimos el cuarto número
+        // Si el número 1 es mayor al número 2 "y" si el número 3 es mayor al número 4.
+        System.out.println(num1 > num2 && num3 > num4 ? "El número " + num1 + " es mayor al " + 
+                            num2 + " y el número " + num3 + " es mayor al número " + num4 :
+                            "No se cumple que el " + num1 + " sea mayor al número " + num2 + 
+                            " y el número " + num3 + " sea mayor al número " + num4);
+        // Si el número 1 es mayor al número 2 o si el número 3 es mayor al número 4.
+        System.out.println(num1 > num2 || num3 > num4? "El número 01 es mayor al número 02 o el número 03 es mayor al número 04.":
+                                                        "El número 01 no es mayor al número 02 y el número 03 no es mayor al número 04.");
+        números.close();
+    }
+}

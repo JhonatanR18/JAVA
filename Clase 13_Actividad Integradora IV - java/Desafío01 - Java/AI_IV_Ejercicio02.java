77a88c10fbdea6406e1a0f701cfa40730c505a61
Jhonatan Rivera <jhontanjuarez723@gmail.com>
2024-07-26 18:10:45 -0500

    Lo que pide la: Actividad Integradora IV || Desafíos I - Serie Fibonacci Mejorada.

--- Clase 13_Actividad Integradora IV - java/Desafío01 - Java/AI_IV_Ejercicio02.java
+++ Clase 13_Actividad Integradora IV - java/Desafío01 - Java/AI_IV_Ejercicio02.java
@@ -1,3 +1,11 @@
+// En esta actividad, te propongo un desafío adicional relacionado con la serie de Fibonacci. La tarea consiste en crear un programa que te permita ingresar la posición de un número en la serie de Fibonacci y, a continuación, mostrar el valor correspondiente a esa posición.
+// Recuerda que la serie de Fibonacci comienza con valores específicos para las primeras posiciones:
+// Posición 0: 0
+// Posición 1: 1
+// Posición 2: 1
+// Posición 3: 2
+// Posición 4: 3
+// Tu objetivo es desarrollar un programa que sea capaz de calcular y mostrar el valor de la serie de Fibonacci para cualquier posición ingresada por el usuario. Este ejercicio te ayudará a consolidar tu comprensión sobre cómo generar y trabajar con la serie de Fibonacci. ¡Anímate a enfrentar este reto y sigue profundizando en tus habilidades de programación!
 import java.util.InputMismatchException;
 import java.util.Scanner;
 public class AI_IV_Ejercicio02 {
